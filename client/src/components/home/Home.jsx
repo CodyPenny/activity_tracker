24989c057b62d843f57a548176fb249722b78e29
Evelyn Rhee <evelynrhee103@gmail.com>
2022-03-24 20:01:13 -0700

    implement loader in home

--- client/src/components/home/Home.jsx
+++ client/src/components/home/Home.jsx
@@ -3,7 +3,7 @@ import { UserContext } from '../providers/UsersProvider.jsx';
 import { useNavigate } from 'react-router-dom';
 
 // Chakra + Forms
-import { Avatar,Grid, GridItem, useBreakpointValue, Flex, Center, useDisclosure } from '@chakra-ui/react';
+import { Avatar,Grid, GridItem, useBreakpointValue, Flex, Center, useDisclosure, Spinner } from '@chakra-ui/react';
 
 import test_pic from '../../../static/avatar.png'
 import NavButton from './NavButton.jsx';
@@ -50,60 +50,71 @@ const Home = () => {
   }, [])
 
   return (
-    <Grid
-    h='100%'
-    bg="brand.100"
-    templateRows='1fr minmax(0, 5fr) 1fr' //prevents row blowout
-    pr="10%" 
-    pl="10%" 
-    >
-      <GridItem 
-        pt={["10%", "5%"]}
-      >
-        <Flex
-          flexDirection="column"
-          alignItems="center"
+    <>
+    {  user.user  ? 
+      (
+        <Grid
+        h='100%'
+        bg="brand.100"
+        templateRows='1fr minmax(0, 5fr) 1fr' //prevents row blowout
+        pr="10%" 
+        pl="10%" 
         >
-          <Avatar
-            name={user.user && user.user.displayName}
-            src={user.user && user.user.photoURL}
-            size={avatarSize}
-            cursor="pointer"
-            onClick={openEditAccountPage}
-          />
-          <EditAccount 
-            isOpen={isOpen}
-            onOpen={onOpen}
-            onClose={onClose}
-            uid={user.user.uid}
-          />
-        </Flex>
-      </GridItem>
-         
-      <GridItem>
-        <Flex
-          flexDir="column"
-          height="100%"
-          gap="1.5rem"
-          pb="1.5rem"
-        >
-          <Center 
-            fontSize={['md', 'xl']}
-            fontWeight="600"
-            pt=".5rem"
+          <GridItem 
+            pt={["10%", "5%"]}
           >
-            {user.user.displayName}
-          </Center>
-          <Stats /> 
-          <ActiveChallenges />
-        </Flex>
-      </GridItem>
-      
-      <GridItem
-      >
-        <NavButton />
-      </GridItem>
-    </Grid>
+            <Flex
+              flexDirection="column"
+              alignItems="center"
+            >
+              <Avatar
+                name={user.user && user.user.displayName}
+                src={user.user && user.user.photoURL}
+                size={avatarSize}
+                cursor="pointer"
+                onClick={openEditAccountPage}
+              />
+              <EditAccount 
+                isOpen={isOpen}
+                onOpen={onOpen}
+                onClose={onClose}
+                uid={user.user.uid}
+              />
+            </Flex>
+          </GridItem>
+            
+          <GridItem>
+            <Flex
+              flexDir="column"
+              height="100%"
+              gap="1.5rem"
+              pb="1.5rem"
+            >
+              <Center 
+                fontSize={['md', 'xl']}
+                fontWeight="600"
+                pt=".5rem"
+              >
+                {user.user.displayName}
+              </Center>
+              <Stats /> 
+              <ActiveChallenges />
+            </Flex>
+          </GridItem>
+          
+          <GridItem
+          >
+            <NavButton />
+          </GridItem>
+        </Grid> 
+    ) : (
+      <>
+         <Spinner size='xl' />
+      </>
+    )
+    
+  }
+  </>
   );
 };
 
